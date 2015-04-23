d14f2ace7551f8607e8d2c9f6242271158d93905
csiu <celia.sy.siu@gmail.com>
2015-04-23 12:52:58 -0700

    redo dat; added label info

--- code/plots.py
+++ code/plots.py
@@ -28,7 +28,7 @@ def _filterPredictionsByClass_reformat2gff(infile, outdir, keep='prom'):
                     out.write(newline + '\n')
     return outfile
 
-def _read_allpairs(gff_infile):
+def _read_dat(gff_infile):
     dat = {}
     n = 0
     with open(gff_infile) as f:
@@ -42,28 +42,31 @@ def _read_allpairs(gff_infile):
             strand = l[6]
             tss = ','.join([chrom, tstart, tstop, strand])
 
-            info     = l[8].split(';')
+            info  = l[8].split(';')
             mirna = get_value_from_keycolonvalue_list('mirbase_id', info)
+            label = get_value_from_keycolonvalue_list('mirna_label', info)
+            if label == '': label = 'NA'
 
             features = l[7].split(';')
             corr = get_value_from_keycolonvalue_list('corr', features)
             if get_value_from_keycolonvalue_list('mirna_prox', features) != '0':
                 distance = get_value_from_keycolonvalue_list('distance', info)
 
-            dat[n] = [tss, mirna, distance, corr]
+            dat[n] = [tss, mirna, label, distance, corr]
 
     dat = pd.DataFrame.from_dict(dat, orient='index')
-    dat.columns = ['tss', 'mirna', 'distance', 'correlation']
+    dat.columns = ['tss', 'mirna', 'label', 'distance', 'correlation']
     return dat
+
 
 def main(infile, outdir):
     outdir = os.path.abspath(outdir)
     ensure_dir(outdir)
 
     infile = _filterPredictionsByClass_reformat2gff(infile, outdir)
-    dat_all_pairs = _read_allpairs(infile)
-    print dat_all_pairs
-    print infile
+    dat = _read_dat(infile)
+    print dat
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=usage,
